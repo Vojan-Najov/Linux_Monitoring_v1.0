c94533b6b469ab53f13e3e0f780aff6a41b3d3e8
Vojan-Najov <serejah.mashichev@gmail.com>
2023-05-30 01:44:20 +0300

    rework 02

--- 02/main.sh
+++ 02/main.sh
@@ -3,19 +3,37 @@
 DIR_PATH=$(dirname $0)
 FILENAME=$(date +"%d_%m_%y_%H_%M_%S.status")
 
+. $DIR_PATH/system_info.sh
+
 function print_info {
-  $DIR_PATH/print_host_info.sh
-  $DIR_PATH/print_date_info.sh
-  $DIR_PATH/print_net_info.sh
-  $DIR_PATH/print_ram_info.sh
-  $DIR_PATH/print_space_info.sh
+  echo "HOSTNAME = $(get_hostname)"
+  echo "TIMEZONE = $(get_timezone)"
+  echo "USER = $USER"
+  echo "OS = $(get_os)"
+  echo "DATE = $(get_date)"
+  echo "UPTIME = $(get_uptime)"
+  echo "UPTIME_SEC = $(get_uptime_sec)"
+  local IP=$(get_ip)
+  IFS='\n' read -ra IP_ARRAY <<< $IP; IP=${IP_ARRAY[0]}
+  echo "IP = $IP"
+  IFS='/' read -ra IP_ARRAY <<< $IP; MASK=${IP_ARRAY[1]}
+  echo "MASK = $(get_netmask $MASK)"
+  echo "GATEWAY = $(get_gateway)"
+  local RAM_INFO=$(get_ram_info)
+  IFS=';' read -ra RAM_ARRAY <<< $RAM_INFO
+  echo "RAM_TOTAL = ${RAM_ARRAY[0]}"
+  echo "RAM_USED = ${RAM_ARRAY[1]}"
+  echo "RAM_FREE = ${RAM_ARRAY[2]}"
+  local SPACE_INFO=$(get_space_info)
+  IFS=';' read -ra SPACE_ARRAY <<< $SPACE_INFO
+  echo "SPACE_ROOT_TOTAL = ${SPACE_ARRAY[0]}"
+  echo "SPACE_ROOT_USED = ${SPACE_ARRAY[1]}"
+  echo "SPACE_ROOT_FREE = ${SPACE_ARRAY[2]}"
 }
 
 print_info
 
-echo "Write data to a file? Y/N"
-read ANSWER
-
+read -p "Write data to a file (Y/N)? " ANSWER
 if [[ "$ANSWER" = "y" || "$ANSWER" = "Y" ]]
 then
   print_info >$FILENAME
---
+++ 02/system_info.sh
@@ -0,0 +1,113 @@
+function get_hostname() {
+  local HOSTNAME=$(cat /etc/hostname)
+
+  echo "$HOSTNAME"
+}
+
+function get_timezone() {
+  local TIMEZONE=$(cat /etc/timezone)
+  local UTC_OFFSET=$(date +%:::z)
+
+  echo "$TIMEZONE UTC $UTC_OFFSET"
+}
+
+function get_os() {
+  local OS=$(uname -o)
+  local OS_NAME=$(cat /etc/os-release | grep ^NAME= | cut -c 6- | tr -d \")
+  local OS_VERSION=$(cat /etc/os-release | grep ^VERSION= | cut -c 9- | tr -d \")
+
+  echo "$OS $OS_NAME $OS_VERSION"
+}
+
+function get_date() {
+  local DATE_FORMAT="%d %b %Y %l:%M:%S"
+  local DATE=$(date +"$DATE_FORMAT")
+
+  echo "$DATE"
+}
+
+function get_uptime() {
+  UPTIME=$(uptime -p | cut -c 4-)
+
+  echo "$UPTIME"
+}
+
+function get_uptime_sec() {
+  local UPTIME_SINCE=$(uptime -s)
+  local UPTIME_SEC_START=$(date --date="$UPTIME_SINCE" +"%s")
+  local UPTIME_SEC_NOW=$(date --date="now" +"%s")
+  local UPTIME_SEC=0
+
+  let "UPTIME_SEC = $UPTIME_SEC_NOW - $UPTIME_SEC_START"
+  echo "$UPTIME_SEC"
+}
+
+function get_ip() {
+  local IP=$(ip -4 address | grep -v lo$ | awk '/inet/{print$2}')
+
+  if [ -z "$IP" ]
+  then
+    IP=$(ip -4 address | awk '/inet/{print$2}')
+  fi
+
+  echo "$IP"
+}
+
+function get_netmask() {
+  #local NETMASK=$(ipcalc $IP | awk '/Netmask/{print$2}')
+  #echo "$NETMASK"
+  local masks=("0.0.0.0" "128.0.0.0" "192.0.0.0"
+               "224.0.0.0" "240.0.0.0" "248.0.0.0"
+               "252.0.0.0" "254.0.0.0" "255.0.0.0"
+               "255.128.0.0" "255.192.0.0" "255.224.0.0"
+               "255.240.0.0" "255.248.0.0" "255.252.0.0"
+               "255.254.0.0" "255.255.0.0" "255.255.128.0"
+               "255.255.192.0"  "255.255.224.0" "255.255.240.0"
+               "255.255.248.0" "255.255.252.0"  "255.255.254.0"
+               "255.255.255.0"  "255.255.255.128"  "255.255.255.192"
+               "255.255.255.224" "255.255.255.240" "255.255.255.248"
+               "255.255.255.252" "255.255.255.254"  "255.255.255.255")
+
+  if [[ $1 -ge 0 && $1 -le 32 ]]
+  then
+    echo ${masks[$1]}
+  fi
+}
+
+function get_gateway() {
+  GATEWAY=$(ip route list | awk '/default/{print$3}')
+
+  echo "$GATEWAY"
+}
+
+function get_ram_info() {
+  local MEM_INFO=$(free -b | grep Mem)
+  local RAM_TOTAL_B=$(echo $MEM_INFO | awk '/Mem/{print$2}')
+  local RAM_USED_B=$(echo $MEM_INFO | awk '/Mem/{print$3}')
+  local RAM_FREE_B=$(echo $MEM_INFO | awk '/Mem/{print$4}')
+  local RAM_TOTAL=$(echo "scale = 3; $RAM_TOTAL_B / 1000.0 / 1000.0 / 1000.0" | \
+                    bc | awk '{printf "%.3f", $0}')
+  local RAM_USED=$(echo "scale = 3; $RAM_USED_B / 1000.0 / 1000.0 / 1000.0" | \
+                   bc | awk '{printf "%.3f", $0}')
+  local RAM_FREE=$(echo "scale = 3; $RAM_FREE_B / 1000.0 / 1000.0 / 1000.0" | \
+                   bc | awk '{printf "%.3f", $0}')
+
+  echo "$RAM_TOTAL GB;$RAM_USED GB;$RAM_FREE GB"
+}
+
+function get_space_info() {
+  local DF_INFO=$(df --block-size=1 / | grep /)
+  local SPACE_ROOT_B=$(echo $DF_INFO | awk '//{print$2}')
+  local SPACE_ROOT_USED_B=$(echo $DF_INFO | awk '//{print$3}')
+  local SPACE_ROOT_FREE_B=$(echo $DF_INFO | awk '//{print$4}')
+  local SPACE_ROOT=$(echo "scale = 2; $SPACE_ROOT_B / 1000.0 / 1000.0" | \
+                     bc | awk '{printf "%.2f", $0}')
+  local SPACE_ROOT_USED=$(echo "scale = 2; $SPACE_ROOT_USED_B / 1000.0 / 1000.0" | \
+                          bc | awk '{printf "%.2f", $0}')
+  local SPACE_ROOT_FREE=$(echo "scale = 2; $SPACE_ROOT_FREE_B / 1000.0 / 1000.0" | \
+                          bc | awk '{printf "%.2f", $0}')
+
+
+  echo "$SPACE_ROOT MB;$SPACE_ROOT_USED MB;$SPACE_ROOT_FREE MB"
+}
+
