39127cfa441d0720e4e7d361506c4cdcdef0291e
Vojan-Najov <serejah.mashichev@gmail.com>
2023-05-29 19:37:51 +0300

    add print_space_info.sh

--- 02/main.sh
+++ 02/main.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 DIR_PATH=$(dirname $0)
+FILENAME=$(date +"%d_%m_%y_%H_%M_%S.status")
 
-$DIR_PATH/print_host_info.sh
-$DIR_PATH/print_date_info.sh
-$DIR_PATH/print_net_info.sh
-$DIR_PATH/print_ram_info.sh
-#./print_space_info.sh
+function print_info {
+  $DIR_PATH/print_host_info.sh
+  $DIR_PATH/print_date_info.sh
+  $DIR_PATH/print_net_info.sh
+  $DIR_PATH/print_ram_info.sh
+  $DIR_PATH/print_space_info.sh
+}
+
+print_info
+
+echo "Write data to a file? Y/N"
+read ANSWER
+
+if [[ "$ANSWER" = "y" || "$ANSWER" = "Y" ]]
+then
+  print_info >$FILENAME
+fi
 
