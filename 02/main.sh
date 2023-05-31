5b91d1882ad8653c7e314d32038aa32f8522ee19
Vojan-Najov <serejah.mashichev@gmail.com>
2023-05-31 17:41:23 +0300

    fix part02

--- 02/main.sh
+++ 02/main.sh
@@ -8,7 +8,7 @@ FILENAME=$(date +"%d_%m_%y_%H_%M_%S.status")
 
 print_info
 
-read -p "Write data to a file (Y/N)? " ANSWER
+echo; read -p "Write data to a file (Y/N)? " ANSWER
 if [[ "$ANSWER" = "y" || "$ANSWER" = "Y" ]]
 then
   print_info >$FILENAME
