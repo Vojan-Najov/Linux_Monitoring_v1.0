5c943d94d577ae91e08afac4ab304af8fbc3221c
Vojan-Najov <serejah.mashichev@gmail.com>
2023-06-04 19:50:23 +0300

    q

--- 04/print_settings.sh
+++ 04/print_settings.sh
@@ -8,18 +8,17 @@ function print_settings() {
   then
     for (( i=0; i < 4; ++i ))
     do
-      case ${COLORS[$i]} in
-        "white") COLOR_TAGS[$i]=1;;
-        "red") COLOR_TAGS[$i]=2;;
-        "green") COLOR_TAGS[$i]=3;;
-        "blue") COLOR_TAGS[$i]=4;;
+      case "${COLORS[$i]}" in
+        "white")  COLOR_TAGS[$i]=1;;
+        "red")    COLOR_TAGS[$i]=2;;
+        "green")  COLOR_TAGS[$i]=3;;
+        "blue")   COLOR_TAGS[$i]=4;;
         "purple") COLOR_TAGS[$i]=5;;
-        "black") COLOR_TAGS[$i]=6;;
+        "black")  COLOR_TAGS[$i]=6;;
       esac
     done
   fi
 
-  printf "\n"
   printf "Column 1 background = %s (%s)\n" "${COLOR_TAGS[0]}" "${COLORS[0]}"
   printf "Column 1 font color = %s (%s)\n" "${COLOR_TAGS[1]}" "${COLORS[1]}"
   printf "Column 2 background = %s (%s)\n" "${COLOR_TAGS[2]}" "${COLORS[2]}"
