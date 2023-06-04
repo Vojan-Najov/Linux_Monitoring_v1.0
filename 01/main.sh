ce7138fc9c25a300c64407179ac8d798543e2c49
Vojan-Najov <serejah.mashichev@gmail.com>
2023-06-04 15:54:47 +0300

    fix part01

--- 01/main.sh
+++ 01/main.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 DIR_PATH=$(dirname $0)
+
 ARG_ERR="Error: the parameter is a number."
 ARG_NUMBER_ERR="Error: one parameter is expected."
 
 . $DIR_PATH/isnumber.sh
 
-if [ $# -eq 1 ]
-then
-  isnumber $1
-  if [ $? -eq 1 ]
-  then
-    echo "$1"
-  else
-    echo "$ARG_ERR"
-  fi
-else
-  echo "$ARG_NUMBER_ERR"
-fi
+if [ $# -ne 1 ]; then echo "$ARG_NUMBER_ERR"; exit 1; fi
+
+isnumber "$1"
+if [ $? -eq 1 ]; then echo "$ARG_ERR"; exit 1; fi
+
+echo "$1"
 
