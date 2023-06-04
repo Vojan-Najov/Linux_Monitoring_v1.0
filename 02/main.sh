106c0e903f00e86a2a39c9a1ce417220bc26889b
Vojan-Najov <serejah.mashichev@gmail.com>
2023-06-04 17:45:48 +0300

    fix part02

--- 02/main.sh
+++ 02/main.sh
@@ -5,11 +5,12 @@ DIR_PATH="$(dirname $0)"
 . "$DIR_PATH"/system_info.sh
 . "$DIR_PATH"/print_info.sh
 
-print_info
+INFO="$(print_info)"
+echo "$INFO"
 
 echo; read -p "Write data to a file (Y/N)? " ANSWER
 if [[ "$ANSWER" = "y" || "$ANSWER" = "Y" ]]; then
   FILENAME="$(date +"%d_%m_%y_%H_%M_%S.status")"
-  print_info >"$FILENAME"
+  echo "$INFO" >"$FILENAME"
 fi
 
